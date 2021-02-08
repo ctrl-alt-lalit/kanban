cf586044db41345524856deb2d4ddfc285686dfb
lalit <labauskar@gmail.com>
2021-02-08 03:27:52 -0600

    board now has persistent state

--- src/board/index.js
+++ src/board/index.js
@@ -1,2 +1,172 @@
-let x = document.getElementById("head1");
-x.innerText = "js works";
+"use strict";
+
+class MessageSender {
+
+    #vscode;
+
+    constructor() {
+        this.#vscode = acquireVsCodeApi();
+    }
+
+    send(command, data) {
+        this.#vscode.postMessage({
+            command: command,
+            data: data
+        });
+    }
+}
+
+const sender = new MessageSender();
+let keysPressed = {};
+
+function addListeners() {
+
+    document.addEventListener("keydown", event => {
+        keysPressed[event.key] = true;
+
+        if (keysPressed["Control"] && event.key === "s") {
+            saveData();
+        }
+    });
+
+    document.addEventListener("keyup", event => {
+        keysPressed[event.key] = false;
+    });
+
+    document.getElementById("save").addEventListener("click", () => {
+        saveData();
+    });
+
+    window.addEventListener("message", event=> { // listen for message
+        const message = event.data;
+        switch (message.command) {
+            case "load":
+                loadData(message.data);
+                break;
+        }
+    });
+
+
+    function saveData() {
+        const columns = document.getElementById("board").children;
+        let data = {};
+        data.ncols = columns.length;
+        data.cols = [];
+
+        for (const column of columns) {
+            let col = {};
+            const children = column.children;
+            col.ntasks = children.length - 1;
+            col.tasks = [];
+
+            for (const child of children) {
+                if (child.className === "header") {
+                    col.title = child.firstElementChild.innerHTML;
+                } else {
+                    col.tasks.push(child.firstElementChild.innerHTML);
+                }
+            }
+
+            data.cols.push(col);
+        }
+
+        sender.send("save", data);
+    }
+
+
+    const columns = document.getElementsByClassName("col");
+
+    for (const col of columns) {
+        const children = col.firstElementChild.children;
+        
+        const addBtn = children[1];
+        const remBtn = children[2];
+
+        addBtn.addEventListener("click", () => { //create new task and add it to bottom of column
+            addTask(col, "Add your own text here!");
+        });
+
+        remBtn.addEventListener("click", () => { //delete col
+            removeColumn(col);
+        });
+    }
+
+    const addCol = document.getElementById("add-col");
+    addCol.addEventListener("click", () => {
+        let board = document.getElementById("board");
+        const column = addColumn(`Column ${board.children.length + 1}`);
+        board.appendChild(column);
+    });
+}
+
+function loadData(data) {
+    let columns = document.getElementsByClassName("col");
+    let board = document.getElementById("board");
+    while (columns.length > 0) {
+        board.removeChild(columns[0]);
+    }
+
+    data.cols.forEach(col => {
+        console.log(JSON.stringify(col));
+        let column = addColumn(col.title);
+        col.tasks.forEach(task => {
+            addTask(column, task);
+        });
+        board.appendChild(column);
+    });
+
+}
+
+function addColumn(title) {
+    let column = document.createElement("div");
+    column.className = "col";
+
+    let header = document.createElement("div");
+    header.className = "header";
+
+    let headerText = document.createElement("h2");
+    headerText.contentEditable = true;
+    headerText.innerHTML = title;
+    
+    let newTask = document.createElement("button");
+    newTask.innerText = "Add Task";
+    newTask.addEventListener("click", () => {
+        addTask(column, "Add your own text here!");
+    });
+
+    let delCol = document.createElement("button");
+    delCol.innerHTML = "Delete Column";
+    delCol.addEventListener("click", () => {
+        removeColumn(column);
+    });
+
+    header.append(headerText, newTask, delCol);
+    column.appendChild(header);
+    return column;
+}
+
+function addTask(column, text) {
+    let task = document.createElement("div");
+    task.className = "task";
+    
+    let taskText = document.createElement("p");
+    taskText.contentEditable = true;
+    taskText.innerHTML = text;
+    
+    let delTask = document.createElement("a");
+    delTask.innerHTML = "Remove";
+    delTask.addEventListener("click", () => {
+        //TODO: Add way to restore task
+        task.remove();
+    });
+
+    task.append(taskText, delTask);
+    column.appendChild(task);
+}
+
+function removeColumn(column) {
+    //TODO: Add way to restore column
+    column.remove();
+}
+
+addListeners();
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,9 @@
 
 import * as vscode from 'vscode'; //contains the VS Code extensibility API
+import { Memento } from "vscode";
 import * as path from "path"; //make path names
 import * as fs from "fs"; //file I/O
+import { TextDecoder, TextEncoder } from 'util';
 var sprintf = require("sprintf-js").sprintf; //format strings (like C)
 
 
@@ -9,13 +11,16 @@ var sprintf = require("sprintf-js").sprintf; //format strings (like C)
 //your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
+	let storage = new StorageManager(context.workspaceState);
+
 	let view = vscode.commands.registerCommand("kanban-board.view", () => {
 		const panel = vscode.window.createWebviewPanel(
 			"kanban-board", //id
 			"Kanban Board", //title
 			vscode.ViewColumn.One, //open at first (leftmost) tab
 			{
-				enableScripts: true //allow JS to be used in webview
+				enableScripts: true, //allow JS to be used in webview
+				retainContextWhenHidden: true //TODO: replace with get/setState at some point
 			}
 		);
 		const webview = panel.webview;
@@ -34,14 +39,96 @@ export function activate(context: vscode.ExtensionContext) {
 
 		//attach URIs to html string and render
 		webview.html = sprintf(htmlString, webview.cspSource.toString(), webview.cspSource.toString(), cssUri.toString(), jsUri.toString());
-		console.log(webview.html);
 
+		createBoard(storage, webview);
+
+		webview.onDidReceiveMessage(message => {
+			switch(message.command) {
+				case "save":
+					storage.store("columns", message.data);
+					break;
+			}
+		});
 	});
 
 	context.subscriptions.push(view);
 }
 
+class StorageManager {
+	constructor(private storage: Memento) {}
 
+	public retrieve<T>(key: string) : T {
+		return this.storage.get<T>(key, null as any);
+	}
+
+	public store<T>(key: string, value: T) {
+		this.storage.update(key, value);
+	}
+}
+
+function createBoard(storage: StorageManager, webview: vscode.Webview) {
+	/*
+	 * EXAMPLE OF COLUMNS OBJECT
+	 * {
+	 *  	ncols: 2,
+	 *  	cols: [
+	 *  		{
+	 * 				title: "column 1"
+	 * 				ntasks: 2,
+	 * 				tasks: [
+	 * 					"task 1",
+	 * 					"task 2",
+	 * 				]
+	 * 			},
+	 * 			{
+	 * 				title: "example"
+	 * 				ntasks: 3,
+	 * 				tasks: [
+	 * 					"foo",
+	 * 					"bar",
+	 * 					"baz"
+	 * 				]
+	 * 			},
+	 * 		],
+	 * }
+	 */
+
+	let columns = storage.retrieve("columns");
+	if (columns === null) {
+		columns = {
+			ncols: 4,
+			cols: [
+				{
+					title: "Bugs",
+					ntasks: 0,
+					tasks: []
+				},
+				{
+					title: "To-Do",
+					ntasks: 1,
+					tasks: [
+						"Put text here!"
+					]
+				},
+				{
+					title: "Doing",
+					ntasks: 0,
+					tasks: []
+				},
+				{
+					title: "Done",
+					ntasks: 0,
+					tasks: []
+				}
+			]
+		};
+	}
+
+	webview.postMessage({
+		command: "load",
+		data: columns
+	});
+}
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
