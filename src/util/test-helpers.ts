b569d14034a28b4ba8d15f7a2e29b525c1ebf68a
lalit <labauskar@gmail.com>
2022-05-21 09:20:26 -0500

    moved ignores to bottonm to silence jsdoc warnings

--- src/util/test-helpers.ts
+++ src/util/test-helpers.ts
@@ -1,9 +1,9 @@
 import userEvent from '@testing-library/user-event';
 
 /**
- * @ignore
  * Wait for a specified number of milliseconds
  * @param ms milliseconds to wait for
+ * @ignore
  */
 export function wait(ms: number) {
     return new Promise((resolve) => {
@@ -12,35 +12,35 @@ export function wait(ms: number) {
 }
 
 /**
- * @ignore
  * @returns a random string of 10 characters
+ * @ignore
  */
 export function randomString() {
     return Math.random().toString(36).slice(0, 10);
 }
 
 /**
- * @ignore
  * Right clicks on an HTMLElement
  * @param element HTMLElement to click on
+ * @ignore
  */
 export function rightClick(element: HTMLElement) {
     userEvent.click(element, { button: 2 });
 }
 
 /**
- * @ignore
  * @returns True or False at random
+ * @ignore
  */
 export function randomBoolean() {
     return Math.random() < 0.5;
 }
 
 /**
- * @ignore
  * @param hi highest number to return (exclusive)
  * @param lo lowest number to return (inclusive)
  * @returns a random integer in the range [lo, hi)
+ * @ignore
  */
 export function randomInteger(hi = 1_000_000_000, lo = 0) {
     return Math.floor(Math.random() * (hi - lo)) + lo;
