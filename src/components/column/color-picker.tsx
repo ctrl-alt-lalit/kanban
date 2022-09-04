97e7cf99d4bfc0383106586858356f85f2b38170
lalit <labauskar@gmail.com>
2022-09-04 16:01:39 -0500

    memoized portions of column color picker

--- src/components/column/color-picker.tsx
+++ src/components/column/color-picker.tsx
@@ -53,6 +53,46 @@ export default function ColorPicker({
     const [textColor, setTextColor] = React.useState(color.slice(1));
     const swatches = boardState.isLightMode ? lightSwatches : darkSwatches;
 
+    const onInputChange = React.useCallback(
+        (event: React.ChangeEvent<HTMLInputElement>) => {
+            const newColor = event.target.value;
+            if (newColor.length > 6 || !/^[\da-fA-F]*$/.test(newColor)) {
+                return;
+            }
+            setTextColor(newColor);
+            console.log(newColor);
+        },
+        [setTextColor]
+    );
+
+    const validateAndChangeColor = React.useCallback(
+        (color: string) => {
+            if (color.length === 3) {
+                changeColor(`#${color[0]}${color[0]}${color[1]}${color[1]}${color[2]}${color[2]}`);
+            } else if (color.length === 6) {
+                changeColor(`#${color}`);
+            }
+        },
+        [changeColor]
+    );
+
+    const makeSwatchButton = React.useCallback(
+        (swatch: string) => (
+            <button
+                key={swatch}
+                className="column-color-picker__swatch"
+                style={{ backgroundColor: swatch }}
+                onClick={() => changeColor(swatch)}
+            />
+        ),
+        []
+    );
+
+    const swatchButtons = React.useMemo(
+        () => swatches.map(makeSwatchButton),
+        [swatches, makeSwatchButton]
+    );
+
     const colorPickerStyle = {
         // CSS styles so that color picker "swipes" open and closed
         maxHeight: isOpen ? '6rem' : 0,
@@ -62,34 +102,13 @@ export default function ColorPicker({
 
     return (
         <div className="column-color-picker" style={colorPickerStyle}>
-            {swatches.map((swatch) => (
-                <button
-                    key={swatch}
-                    className="column-color-picker__swatch"
-                    style={{ backgroundColor: swatch }}
-                    onClick={() => changeColor(swatch)}
-                />
-            ))}
+            {swatchButtons}
             <div className="text-picker">
                 <div className="input-tag"> # </div>
                 <input
                     value={textColor}
-                    onChange={(e) => {
-                        const newColor = e.target.value;
-                        if (newColor.length > 6 || !/^[\da-fA-F]*$/.test(newColor)) {
-                            return;
-                        }
-                        setTextColor(newColor);
-                    }}
-                    onBlur={() => {
-                        if (textColor.length === 3) {
-                            changeColor(
-                                `#${textColor[0]}${textColor[0]}${textColor[1]}${textColor[1]}${textColor[2]}${textColor[2]}`
-                            );
-                        } else if (textColor.length === 6) {
-                            changeColor(`#${textColor}`);
-                        }
-                    }}
+                    onChange={onInputChange}
+                    onBlur={() => validateAndChangeColor(textColor)}
                 />
             </div>
         </div>
